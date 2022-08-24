05fbfed8e7bbc48d3aaa7025ce9347d15a063991
kelvyn777 <kelvynsabino10@gmail.com>
2022-08-24 11:40:43 -0300

    Atividade de banco de dados relacional Mysql

---
+++ MySQL/db_escola.sql
@@ -0,0 +1,31 @@
+/*Exemplo aula*/
+
+
+CREATE DATABASE db_escola;
+
+USE db_escola;
+
+CREATE TABLE tb_alunos(
+
+
+id bigint auto_increment primary key,
+nome varchar (55),
+data_nascimento date,
+turma int,
+nota decimal(4,2)
+
+);
+
+SELECT * FROM tb_alunos;
+
+INSERT INTO tb_alunos(nome,data_nascimento,turma,nota)VALUES("paozin","2003-04-6",57, 9.9);
+
+SELECT * FROM tb_alunos;
+
+SELECT * FROM tb_alunos WHERE nota >7;
+
+SELECT * FROM tb_alunos WHERE nota <7;
+
+
+UPDATE tb_alunos SET nota = 6 WHERE id=8
+
